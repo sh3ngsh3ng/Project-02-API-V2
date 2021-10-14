bf5f6df198dc1b6e01bd10f71712a3c017b016ea
sh3ngsh3ng <yshenglim13@gmail.com>
2021-10-14 14:03:40 +0000

    mongodata base connected to server

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express")
+const cors = require("cors")
 const MongoUtil = require("./MongoUtil.js")
-
+const ObjectId = require("mongodb").ObjectId
 require("dotenv").config()
+const mongoUrl = process.env.MONGO_URL
 
 
 let app = express()
 
+app.use(express.json())
+
+app.use(cors())
 
 
+
+async function main () {
+    let db = await MongoUtil.connect(mongoUrl, "project_02")
+
+}
+
+
+main ()
 
 
 
