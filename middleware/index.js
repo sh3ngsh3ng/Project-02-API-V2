8e93e94ffe4612f431f7c4183433bff7188824e2
sh3ngsh3ng <yshenglim13@gmail.com>
2022-01-06 18:36:12 +0000

    save

--- middleware/index.js
+++ middleware/index.js
@@ -2,9 +2,8 @@
 
 
 
-// Check all fields are filled (see what i did there? hahah)
+// Check that all FIELDS are FILLED when creating questions (see what i did there? hahah)
 const checkFields = (req,res,next) => {
-    console.log(req.body)
     if (req.body.level == null 
         || req.body.grade == null 
         || req.body.subject == null 
@@ -16,14 +15,13 @@ const checkFields = (req,res,next) => {
     } else {
         next()
     }
-    
 }
 
 
 
 module.exports = {checkFields}
 
-// After implement login
-// const checkForAdmin = (req,res,next) => {
+// After implementing login
+// const authenticateUser = (req,res,next) => {
 
 // }
